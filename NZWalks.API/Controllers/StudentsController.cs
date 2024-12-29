d470774627c982cf3d2d5656210994fe7efe446f
Alparslan Tarkan <80176136+SarEnderTarkanson@users.noreply.github.com>
2024-12-29 04:57:33 +0100

    Added a new basic endpoint students

---
+++ NZWalks.API/Controllers/StudentsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NZWalks.API.Controllers
+{
+    // https://localhost:7003/api/students
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        //GET: https://localhost:7003/api/students
+        [HttpGet]
+        public IActionResult GetAllStudents()
+        {
+            string[] studentNames = new string[] { "John", "Jane", "Mark", "Emily", "David" };
+
+            return Ok(studentNames);
+        }
+    }
+}
