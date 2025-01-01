8d3f69ada538b2cbb1d019401b4e93b4645cd9ea
Alparslan Tarkan <80176136+SarEnderTarkanson@users.noreply.github.com>
2025-01-01 04:28:54 +0100

    Added a new controller (regions endpoint)

---
+++ NZWalks.API/Controllers/RegionsController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegionsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var regions = new List<Region>
+            {
+                new Region
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Auckland Region",
+                    Code = "AKL",
+                    RegionImageUrl = "https://www.pexels.com/photo/scenic-view-of-tuscan-countryside-at-sunset-29990367/"
+                },
+                new Region
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Wellington Region",
+                    Code = "WLG",
+                    RegionImageUrl = "https://www.pexels.com/photo/serene-forest-landscape-in-bolu-turkiye-29978190/"
+                }
+            };
+            return Ok(regions);
+        }
+    }
+}
